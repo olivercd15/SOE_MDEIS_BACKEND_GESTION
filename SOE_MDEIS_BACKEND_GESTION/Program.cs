47a12b4d755d5eeeb5e6512f106c98f1d12f6fec
Oliver Carranza <olivercd15@gmail.com>
2025-12-01 12:45:22 -0400

    fix swagger production

--- SOE_MDEIS_BACKEND_GESTION/Program.cs
+++ SOE_MDEIS_BACKEND_GESTION/Program.cs
@@ -29,14 +29,11 @@ builder.Services.AddScoped<IEmpleadoService, EmpleadoService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
+
 
 app.UseAuthorization();
 
